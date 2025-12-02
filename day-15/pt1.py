2e5522e4adee5e1616fa0766c5faa1bdab2b49ab
Hyunwhan <hyunwhanjoe@gmail.com>
2025-12-02 10:11:25 +0000

    day-15/pt1.py - if @ O > O

--- day-15/pt1.py
+++ day-15/pt1.py
@@ -5,20 +5,35 @@ def main(f_name):
     map = []
     robot_pos = [0,0]
     
+    def _read_map(line, i):
+        map.append([])
+        
+        for j, letter in enumerate(line.strip()):
+            if letter == '#' or letter == '@' or letter == '.' or letter == 'O':
+                map[i].append(letter)
+                
+                if letter == '@':
+                    robot_pos[0] = i
+                    robot_pos[1] = j
+            else:
+                print('invalid letter')
+                breakpoint()
+        
     def _move_robot(row, col):
         map[robot_pos[0]] [robot_pos[1]] = '.'
         robot_pos[0] = row
         robot_pos[1] = col
         map[row][col] = '@'
         
-    def _read_map(line, i):
-        map.append([])
-        for j, letter in enumerate(line.strip()):
-            map[i].append(letter)
-            
-            if letter == '@':
-                robot_pos[0] = i
-                robot_pos[1] = j
+    def _check_wall(row, col):
+        # '>'
+        k = 1
+        while map[row][col+k] == 'O':
+            k += 1
+            if map[row][col+k] == '#':
+                return True
+        
+        return False
     
     with open(f_name, "r") as f:
         for i, line in enumerate(f):
@@ -47,9 +62,6 @@ def main(f_name):
                                 _move_robot(row, col)
                                 map[row][col-1] = 'O'
                             # elif map[row][col-1] == 'O':
-                            else:
-                                print('Not valid piece')
-                                break
                     
                     elif direction == '>':
                         row = robot_pos[0]
@@ -58,7 +70,7 @@ def main(f_name):
                         if map[row][col] == '#': 
                             continue
                         elif map[row][col] == '.':
-                            _move_robot()
+                            _move_robot(row, col)
                         
                         elif map[row][col] == 'O':
                             if map[row][col+1] == '#':
@@ -66,11 +78,17 @@ def main(f_name):
                             elif map[row][col+1] == '.':
                                 _move_robot(row, col)
                                 map[row][col+1] = 'O'
-                            # elif map[row][col+1] == 'O':
-                            else:
-                                print('Not valid piece')
-                                break
-                                
+                            
+                            elif map[row][col+1] == 'O':
+                                if _check_wall(row, col):
+                                    continue
+                                else:
+                                    k = 0
+                                    while map[row][col+k] != '.':
+                                        k+= 1
+                                    
+                                    map[row][col+k] = 'O'
+                                    _move_robot(row, col)
                     
                     elif direction == '^':
                         row = robot_pos[0] - 1
@@ -88,9 +106,6 @@ def main(f_name):
                                 _move_robot(row, col)
                                 map[row-1][col] = 'O'
                             # elif map[row-1][col] == 'O':
-                            else:
-                                print('Not valid piece')
-                                break
                 
                     elif direction == 'v':
                         row = robot_pos[0] + 1
@@ -108,9 +123,7 @@ def main(f_name):
                                 _move_robot(row, col)
                                 map[row+1][col] = 'O'
                             # elif map[row+1][col] == 'O':
-                            else:
-                                print('Not valid piece')
-                                break
+
                     
                     else:
                         print('Not a valid direction')
